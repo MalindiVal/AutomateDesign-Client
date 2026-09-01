64d39722b3d450c3f0f91d03fb481535f38d9353
MalindiVal <63977034+MalindiVal@users.noreply.github.com>
2026-09-01 05:33:59 +0200

    Création du gardien

---
+++ LogicLayer/AutomateKeeper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+    public class AutomateKeeper
+    {
+        private Stack<AutomateMemento> states = new Stack<AutomateMemento>();
+        private Automate automate;
+
+        public void Keep(Automate p)
+        {
+            states.Clear();
+            automate = p;
+        }
+        public void Do()
+        {
+            states.Push(automate.Save());
+        }
+        public void Undo()
+        {
+            automate.Restore(states.Pop());
+        }
+    }
+}
