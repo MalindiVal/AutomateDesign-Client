6d2bdefc316f6a697148c5c6b69b821bca02f827
MalindiVal <63977034+MalindiVal@users.noreply.github.com>
2026-09-01 05:25:43 +0200

    Create AutomateMemento.cs

---
+++ LogicLayer/AutomateMemento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+    public class AutomateMemento
+    {
+        internal string nom;
+        internal List<Etat> etats;
+        internal List<Transition> transitions;
+        internal AutomateMemento(string nom, List<Etat> etats, List<Transition> transitions)
+        {
+            this.nom = nom;
+            this.etats = new List<Etat>(etats);
+            this.transitions = new List<Transition>(transitions);
+
+        }
+    }
+}
